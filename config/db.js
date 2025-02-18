46776a1a201942be48a5444bbfd10c8fd697bed3
WasaPumpkin <andycarsan10@gmail.com>
2025-02-18 12:29:44 -0500

    Fix: Removed deprecated options from MongoDB connection

--- config/db.js
+++ config/db.js
@@ -5,10 +5,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI); // No need for options
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
@@ -17,6 +14,7 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
 
 
 
