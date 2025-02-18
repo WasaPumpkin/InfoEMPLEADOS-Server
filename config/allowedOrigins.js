edb6031f56be64177180418620792cce13781c9c
WasaPumpkin <andycarsan10@gmail.com>
2025-02-18 13:29:58 -0500

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,9 +2,9 @@
 
 // src/config/allowedOrigins.js
 const allowedOrigins = [
-  'http://localhost:5173', // Frontend URL for local development
-  'http://localhost:3000',//Frontend URL for local development
-  'https://infoempleados.onrender.com', // Production frontend URL
+  // 'http://localhost:5173', // Frontend URL for local development
+  // 'http://localhost:3000',//Frontend URL for local development
+  'https://infoempleados-server.onrender.com'
 ];
 
 export default allowedOrigins;
